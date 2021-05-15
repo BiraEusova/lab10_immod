d3b8bf7bc2c0496c9c9d54f5fc436d0427574700
Ka3achka <ira.breusova@gmail.com>
2021-05-16 02:50:11 +0700

    lab 10 was done

---
+++ lab8_immod/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace lab8_immod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelError = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonGetAnswerOrb = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.labelRes = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Location = new System.Drawing.Point(41, 465);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(0, 20);
+            this.labelError.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Info;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(346, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 37);
+            this.label2.TabIndex = 5;
+            // 
+            // buttonGetAnswerOrb
+            // 
+            this.buttonGetAnswerOrb.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonGetAnswerOrb.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonGetAnswerOrb.Location = new System.Drawing.Point(213, 117);
+            this.buttonGetAnswerOrb.Name = "buttonGetAnswerOrb";
+            this.buttonGetAnswerOrb.Size = new System.Drawing.Size(210, 53);
+            this.buttonGetAnswerOrb.TabIndex = 6;
+            this.buttonGetAnswerOrb.Text = "Roll the dice";
+            this.buttonGetAnswerOrb.UseVisualStyleBackColor = false;
+            this.buttonGetAnswerOrb.Click += new System.EventHandler(this.buttonGetAnswerOrb_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(166, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Number of dice faces";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(209, 199);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 20);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Result:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(346, 56);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 12;
+            // 
+            // labelRes
+            // 
+            this.labelRes.AutoSize = true;
+            this.labelRes.Location = new System.Drawing.Point(274, 199);
+            this.labelRes.Name = "labelRes";
+            this.labelRes.Size = new System.Drawing.Size(0, 20);
+            this.labelRes.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 546);
+            this.Controls.Add(this.labelRes);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonGetAnswerOrb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelError);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonGetAnswerOrb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label labelRes;
+    }
+}
+
---
+++ lab8_immod/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab8_immod
+{
+    public partial class Form1 : Form
+    {
+        private DiceRoll diceRoll;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGetAnswerOrb_Click(object sender, EventArgs e)
+        {          
+            ReadData();
+            labelRes.Text = diceRoll.GetRolledSide().ToString(); 
+        }
+
+        void ReadData()
+        {
+            var N = (int)numericUpDown1.Value;
+            diceRoll = new DiceRoll(N);
+            diceRoll.GenerateProbArray();
+        }
+
+    }
+}
